402a0055a373c78b7a1625222c828981010c98f1
Cristopher Paiva <josuepaiva7@gmail.com>
2023-06-20 22:23:55 -0300

    Add files via upload

--- crud-jdbc/src/App.java
+++ crud-jdbc/src/App.java
@@ -2,10 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String sql = "select * from funcionario";
+    public static void main(String[] args) throws SQLException {
+
+        String sql = "SELECT * FROM funcionario";
+        String sql2 = "INSERT INTO funcionario(nome, email, idade) values('cristopher', 'cristopher@gmail.com', 25);";
+        String sql3 = "UPDATE funcionario SET nome = 'josue', email = 'josue@hotmail.es' WHERE id_func = 4;" ;
+        String sql4 = "DELETE from funcionario WHERE id_func = 6;" ;
         String url = "jdbc:mysql://localhost:3306/empresa";
         String user = "root";
         String password = "";
@@ -14,21 +19,35 @@ public class App {
 
             Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement stm = conn.prepareStatement(sql);
+            PreparedStatement stm2 = conn.prepareStatement(sql2);
+            PreparedStatement stm3 = conn.prepareStatement(sql3);
+            PreparedStatement stm4 = conn.prepareStatement(sql4);
+            
+
+            //delete
+            stm4.executeUpdate();
+
+            //update
+            stm3.executeUpdate();
+
+
+            //insert into
+            // stm2.executeUpdate();
+
+
+            // selct
             ResultSet rs = stm.executeQuery();
-
-
             while (rs.next()) {
-                System.out.println(rs.getString("nome"));
+                System.out.println("id:" + rs.getString("id_func") + " nome:" + rs.getString("nome") + " email: " + rs.getString("email") + " idade: " + rs.getString("idade"));
             }
 
-            rs.close();
 
-            stm.close();
-
-
-        } catch (Exception e) {
+            // rs.close();
+            // stm.close();
+        } catch (SQLException e) {
             
             e.printStackTrace();
+    
         }
        
     }
